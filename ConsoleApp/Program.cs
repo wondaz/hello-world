d3b8cb248e22323cdb18e45b2e29aa636949b0b6
wdz <65917154@qq.com>
2022-07-13 21:43:38 +0800

    test

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,37 @@
+﻿using Frame.Utils;
+using System;
+using System.Collections.Generic;
+using System.Data;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            var s1 = new Student()
+            {
+                stuAge = 1,
+                stuGrade = "A",
+                stuName = "wang"
+            };
+            var s2 = new Student()
+            {
+                stuAge = 21,
+                stuGrade = "BA",
+                stuName = "DAZHI"
+            };
+
+            var list = new List<Student>();
+            list[0] = s1;
+            list[1] = s2;
+
+            DataTable dt = ZConvert.ToDataTable<Student>(list);
+            foreach(var r in dt.Rows)
+            {
+
+            }
+        }
+    }
+}
---
+++ ConsoleApp/Student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Student
+    {
+        public string stuName { get; set; }
+        public int stuAge { get; set; }
+        public string stuGrade{get;set;}
+    }
+}
